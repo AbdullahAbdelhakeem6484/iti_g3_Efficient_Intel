7da9b0aff5085577af144c55c216c3cb642d97fa
abdullah abdelhakeem <abdullah.abdelhakeem25@gmail.com>
2021-09-13 13:51:13 +0200

    first directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='flexible project structure for doing and sharing data science',
+    author='Abdullah_Abdelhakeem',
+    license='',
+)
